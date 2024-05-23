2918e0ba46880f7bcd3f857c2f5e6d3016195cf2
umerahmedd <meru5350@gmail.com>
2024-05-23 13:27:29 +0500

    v1.0

--- contracts/sample.sol
+++ contracts/sample.sol
@@ -15,5 +15,22 @@ contract ownable {
         _;
     }
 
-    
+    function setOwner(address _newOwner) public onlyOwner{
+        require(_newOwner != address(0), "Invalid address");
+        owner = _newOwner;
+    }
+
+    function loop() public pure returns(uint){
+        uint count = 0;
+
+        for (uint i=0; i<10; i++) 
+        {
+            if (i == 8){
+                continue; // skips all code below and goes back up
+                //break;  // exits loop g
+            }
+            count = count + 2;
+        }
+        return count;
+    }
 }
