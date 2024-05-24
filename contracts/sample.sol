1f1b75ef9df062078836ad0a225af9b7ba9a8707
umerahmedd <meru5350@gmail.com>
2024-05-24 20:17:07 +0500

    v1.0

--- contracts/sample.sol
+++ contracts/sample.sol
@@ -5,9 +5,12 @@ pragma solidity >=0.8.2 <0.9.0;
 contract ownable {
 
     address public owner;
+    uint[5] public arr = [1,2,3,4,5];
+    uint[5] public arr2;
 
     constructor(){
         owner = msg.sender;
+        arr2 = [10,20,30,40,50];
     }
 
     modifier onlyOwner(){
@@ -33,4 +36,40 @@ contract ownable {
         }
         return count;
     }
+
+    function array() public returns (uint){
+        //get
+        uint temp;
+        temp = arr[3];
+
+        //update
+        arr[2] = 3000;
+
+        //delete
+        delete arr[4];
+
+        //length
+        uint len = arr.length;
+
+        return len; 
+    }
+
+    function arrayWithLoop(uint _x) public {
+        for (uint i = 0; i < arr2.length; i++) 
+        {
+            arr2[i] = _x+i;
+        }
+    }
+
+    function createArray() public pure returns (uint) {
+        uint[] memory arrMem = new uint[](3);
+        arrMem[1] = 33;
+
+        return arrMem[1];
+    }
+
+    function returnArray() public view returns (uint[5] memory) {
+        return arr2;
+    }
+
 }
