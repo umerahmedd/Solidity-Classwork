2a46896a6a9571a53d310572f202b4dbf653d588
umerahmedd <meru5350@gmail.com>
2024-05-24 21:08:31 +0500

    v1.0

--- contracts/sample.sol
+++ contracts/sample.sol
@@ -2,15 +2,39 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract ownable {
+contract Practice {
 
     address public owner;
     uint[5] public arr = [1,2,3,4,5];
     uint[5] public arr2;
+    uint[] public dynamicArr = [10,299,3244,454,43,43,1];
+
+    enum Status {
+        pending, 
+        shipped,
+        accepted,
+        rejected,
+        canceled 
+    }
+
+    Status status;
+
+    struct Emp{
+        string name;
+        uint age;
+        address acc;
+    }
+
+    Emp public emp;
+    Emp[] public emps;
 
     constructor(){
         owner = msg.sender;
         arr2 = [10,20,30,40,50];
+
+        emp.name = "Test";
+        emp.age = 20;
+        emp.acc = owner;
     }
 
     modifier onlyOwner(){
@@ -36,6 +60,8 @@ contract ownable {
         }
         return count;
     }
+
+    // Fixed sized array
 
     function array() public returns (uint){
         //get
@@ -72,4 +98,75 @@ contract ownable {
         return arr2;
     }
 
+    // Dynamic Array
+
+    function dynamicArray() public{
+        
+        // //get
+        // uint temp = dynamicArr[3];
+
+        // //update
+        // dynamicArr[4] = 222;
+
+        // //delete
+        // delete dynamicArr[1];
+
+        // //length
+        // uint len = dynamicArr.length;
+
+        //push
+        dynamicArr.push(99);
+
+        //pop
+        dynamicArr.pop;
+
+    }
+
+    //Enums
+
+    function getStatus() public view returns (Status) {
+        return status;
+    }
+
+    function setStatus(Status _status) public {
+        status = _status;
+    }
+
+    function reject() public {
+        status = Status.rejected;
+    }
+
+    function reset() public {
+        delete status;
+    }
+
+    //Struct
+
+    function setValues() public {
+        
+        //1st way of initializing
+        Emp memory emp1 = Emp("Umer",21, 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+
+        //2nd way of initializing
+        Emp memory emp2 = Emp({acc: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, age:22, name: "Ahmed"});
+
+        //3rd way of initializing
+        Emp memory emp3;
+        emp3.name = "John";
+        emp3.age = 23;
+        emp3.acc = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+        emps.push(emp1);
+        emps.push(emp2);
+        emps.push(emp3);
+
+        emps.push(Emp("Doe",30,msg.sender));
+
+        Emp storage emp_temp = emp; //Storage is used to store data permanently on the blockchain. Memory is used to store temporary data that is needed during the execution of a function.
+        emp_temp.name = "James";
+
+        Emp storage emp_temp2 = emps[1];
+        emp_temp2.age = 31;
+
+    }
 }
